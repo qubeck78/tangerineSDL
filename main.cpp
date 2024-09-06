21c242949281d00ffe5c95262d8af11e479054e2
Michal Kubecki <qubeck78@wp.pl>
2024-09-06 09:59:49 +0200

    stdint types for 64-bit compatibility

--- main.cpp
+++ main.cpp
@@ -50,7 +50,7 @@ int main( int argc,  char** argv )
    cpuctx.storeData        = storeData;
 
    //sd card
-   if( sdcInit( &tgctx.sdCardContext, (char*)"sdcardPrv.img" ) )
+   if( sdcInit( &tgctx.sdCardContext, (char*)"sdcard.img" ) )
    {
       printf( "Can't load sd card image. Ensure srcard.img is in the same dir as emulator executable.\n" );      
    }
